5109ac92b0deb9bae1e523f68c984449837d4552
Mike Schutte <tmikeschutte@gmail.com>
2018-08-26 10:59:54 -0400

    Finish elixir roman numerals

---
+++ elixir/roman-numerals/roman.exs
@@ -0,0 +1,38 @@
+defmodule Roman do
+  @arabic_roman [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  ]
+
+  @doc """
+  Convert the number to a roman number.
+  """
+  @spec numerals(pos_integer) :: String.t()
+
+  def numerals(x) do
+    numerals("", x)
+  end
+
+  @spec numerals(String.t(), pos_integer) :: String.t()
+  def numerals(y, 0), do: y
+
+  def numerals(y, x) do
+    @arabic_roman
+    |> Enum.find(fn {a, _} -> x >= a end)
+    |> reducer(y, x)
+  end
+
+  @spec reducer({pos_integer, String.t()}, String.t(), pos_integer) :: String.t()
+  defp reducer({a, r}, y, x), do: numerals(y <> r, x - a)
+end
