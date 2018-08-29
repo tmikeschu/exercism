479b90ba51bd3d299cb1e3e319dc457f0f3385a0
Mike Schutte <tmikeschutte@gmail.com>
2018-08-28 20:20:18 -0400

    Use union types in elm bob

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,8 +1,50 @@
 module Bob exposing (hey)
 
-import List exposing (all, map)
 import Regex exposing (contains, regex)
 import String exposing (endsWith, isEmpty, toUpper, trim)
+
+
+hey : String -> String
+hey remark =
+    case classifyRemark remark of
+        Just ShoutingQuesiton ->
+            "Calm down, I know what I'm doing!"
+
+        Just Shout ->
+            "Whoa, chill out!"
+
+        Just Question ->
+            "Sure."
+
+        Just Silent ->
+            "Fine. Be that way!"
+
+        Nothing ->
+            "Whatever."
+
+
+type Remark
+    = ShoutingQuesiton
+    | Shout
+    | Question
+    | Silent
+
+
+classifications : List ( Remark, StringPred )
+classifications =
+    [ ( ShoutingQuesiton, isShoutingQuestion )
+    , ( Shout, isShouting )
+    , ( Question, isQuestion )
+    , ( Silent, isSilent )
+    ]
+
+
+classifyRemark : String -> Maybe Remark
+classifyRemark s =
+    classifications
+        |> List.filter (Tuple.second >> (|>) s)
+        |> List.head
+        |> Maybe.map Tuple.first
 
 
 type alias StringPred =
@@ -24,28 +66,11 @@ isQuestion =
     endsWith "?"
 
 
+isShoutingQuestion : StringPred
+isShoutingQuestion s =
+    isShouting s && isQuestion s
+
+
 isSilent : StringPred
 isSilent =
     trim >> isEmpty
-
-
-hey : String -> String
-hey remark =
-    let
-        fact =
-            all ((|>) remark)
-    in
-    if fact [ isShouting, isQuestion ] then
-        "Calm down, I know what I'm doing!"
-
-    else if fact [ isShouting ] then
-        "Whoa, chill out!"
-
-    else if fact [ isQuestion ] then
-        "Sure."
-
-    else if fact [ isSilent ] then
-        "Fine. Be that way!"
-
-    else
-        "Whatever."
