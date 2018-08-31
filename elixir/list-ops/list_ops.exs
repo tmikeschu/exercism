6787370e7ac898e45354c2c9520fdbe7025582c5
Mike Schutte <tmikeschutte@gmail.com>
2018-08-31 08:18:24 -0600

    Improve elixir list ops readability

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -6,11 +6,28 @@ defmodule ListOps do
   def reverse(l), do: reduce(l, [], &[&1 | &2])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f), do: reduce(l, [], &[f.(&1) | &2]) |> reverse
+  def map(l, f) do
+    l
+    |> reduce([], &[f.(&1) | &2])
+    |> reverse
+  end
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f), do: reduce(l, [], filter_predicate(f)) |> reverse
-  defp filter_predicate(f), do: &if(f.(&1), do: [&1 | &2], else: &2)
+  def filter(l, f) do
+    l
+    |> reduce([], filter_predicate(f))
+    |> reverse
+  end
+
+  defp filter_predicate(f) do
+    fn el, acc ->
+      if f.(el) do
+        [el | acc]
+      else
+        acc
+      end
+    end
+  end
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
@@ -22,7 +39,8 @@ defmodule ListOps do
 
   @spec concat([[any]]) :: [any]
   def concat(l), do: concat(l, [])
-  defp concat([], acc), do: acc |> reverse
+  defp concat([], acc), do: reverse(acc)
   defp concat([[] | t], acc), do: concat(t, acc)
+
   defp concat([[h | ht] | t], acc), do: concat([ht | t], [h | acc])
 end
