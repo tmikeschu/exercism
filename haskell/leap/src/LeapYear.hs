ae11d5f5dd7ec58f5c346a842bf8d99793280203
Mike Schutte <tmikeschutte@gmail.com>
2018-10-03 11:51:23 -0400

    Use guards for haskell leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -4,6 +4,8 @@ isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy a b = a `mod` b == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year =
-    year `isDivisibleBy` 4 &&
-      (not (year `isDivisibleBy` 100) || year `isDivisibleBy` 400)
+isLeapYear year
+  | year `isDivisibleBy` 400 = True
+  | year `isDivisibleBy` 100 = False
+  | year `isDivisibleBy` 4 = True
+  | otherwise = False
