0140eab8c1a00deb95f47235fc780734d7a287c9
Mike Schutte <tmikeschutte@gmail.com>
2018-10-03 10:52:20 -0400

    Refactor haskell leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,8 +1,9 @@
 module LeapYear (isLeapYear) where
 
-divBy :: Integer -> Integer -> Bool
-divBy a b = a `mod` b == 0
+isDivisibleBy :: Integer -> Integer -> Bool
+isDivisibleBy a b = a `mod` b == 0
 
 isLeapYear :: Integer -> Bool
-isLeapYear year = yearDivBy 4 && (not (yearDivBy 100) || yearDivBy 400)
-  where yearDivBy = divBy year
+isLeapYear year =
+    year `isDivisibleBy` 4 &&
+      (not (year `isDivisibleBy` 100) || year `isDivisibleBy` 400)
