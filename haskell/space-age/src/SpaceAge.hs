4c82e8f3d5b1fcfc199b5bf9787e7d279d5bcd15
Mike Schutte <tmikeschutte@gmail.com>
2018-10-04 08:24:53 -0400

    Refactor haskell space age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,16 +10,14 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = case planet of
-  Mercury -> convert 0.2408467
-  Venus -> convert 0.61519726
-  Earth -> convert 1
-  Mars -> convert 1.8808158
-  Jupiter -> convert 11.862615
-  Saturn -> convert 29.447498
-  Uranus -> convert 84.016846
-  Neptune -> convert 164.79132
-
-  where earthSecondsTimes = (*) 31557600
-        secondsOver = (/) seconds
-        convert = secondsOver . earthSecondsTimes
+ageOn planet seconds = convert $ case planet of
+    Mercury -> 0.2408467
+    Venus -> 0.61519726
+    Earth -> 1
+    Mars -> 1.8808158
+    Jupiter -> 11.862615
+    Saturn -> 29.447498
+    Uranus -> 84.016846
+    Neptune -> 164.79132
+  where
+    convert = (/) seconds . (*) 31557600
