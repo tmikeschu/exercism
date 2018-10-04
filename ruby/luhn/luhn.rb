aeed08ef1dffa38951d2affd6d2b475bd088b5c3
Mike Schutte <tmikeschutte@gmail.com>
2018-10-04 12:00:31 -0400

    Refactor ruby luhn w/ attr reader

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,39 +1,44 @@
 class Luhn
-  class << self
-    VALIDATORS = %i(valid_length? only_digits? valid_sum?)
+  def self.valid?(raw)
+    new(raw).valid?
+  end
 
-    def valid?(raw)
-      VALIDATORS.all? { |predicate| send(predicate, raw.tr(" ", "")) }
-    end
+  def valid?
+    valid_length? && only_digits? && valid_sum?
+  end
 
-    private
+  private
 
-    def valid_length?(raw)
-      raw.length > 1
-    end
+  attr_reader :sanitized
+  def initialize(raw)
+    @sanitized = raw.tr(" ", "")
+  end
 
-    def only_digits?(raw)
-      !/[^\d^\s]/.===(raw)
-    end
+  def valid_length?
+    sanitized.length > 1
+  end
 
-    def valid_sum?(raw)
-      luhn_sum(raw) % 10 == 0
-    end
+  def only_digits?
+    !/[^\d]/.===(sanitized)
+  end
 
-    def luhn_sum(raw)
-      raw.
-        chars.
-        reverse.
-        map.with_index(&luhn_value).
-        sum(&luhn_doubled)
-    end
+  def valid_sum?
+    (luhn_sum % 10).zero?
+  end
 
-    def luhn_value
-      -> x, index { x.to_i * (index % 2 + 1) }
-    end
+  def luhn_sum
+    sanitized.
+      chars.
+      reverse.
+      map.with_index(&luhn_value).
+      sum(&luhn_doubled)
+  end
 
-    def luhn_doubled
-      -> x { x - (9 * (x / 10)) }
-    end
+  def luhn_value
+    -> x, index { x.to_i * (index % 2 + 1) }
+  end
+
+  def luhn_doubled
+    -> x { x - (9 * (x / 10)) }
   end
 end
