e1a89e285e0714927ecb69752c1f44122c3ea386
Mike Schutte <tmikeschutte@gmail.com>
2018-10-11 23:06:21 -0400

    Return ruby acronym back to SRP

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,7 +1,24 @@
 class Acronym
   def self.abbreviate(phrase)
-    phrase.split(/[,\-\s]/).reduce("") do |acc, word|
-      acc.concat(word.chars.fetch(0, "").upcase)
-    end
+    new(phrase).send(:abbreviate)
+  end
+
+  attr_reader :phrase
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def abbreviate
+    phrase.
+      split(/[,\-\s]/).
+      reduce("", &abbreviate_reducer)
+  end
+  
+  def abbreviate_reducer
+    -> acc, word { acc + first_upcase(word) }
+  end
+
+  def first_upcase(word)
+    word[0].to_s.upcase
   end
 end
