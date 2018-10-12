fbd034203beb4e5184f4fdb971a0432764101e44
Mike Schutte <tmikeschutte@gmail.com>
2018-10-11 22:13:52 -0400

    Simplify ruby acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,25 +1,7 @@
 class Acronym
   def self.abbreviate(phrase)
-    new(phrase).send(:abbreviate)
-  end
-
-  private
-
-  attr_reader :phrase
-  def initialize(phrase)
-    @phrase = phrase
-  end
-
-  def abbreviate
-    phrase.
-      split(/[,\-\s]/).
-      reduce("") { |acc, word| acc + abbreviate_word(word) }
-  end
-
-  def abbreviate_word(word)
-    word.
-      chars.
-      fetch(0, "").
-      upcase
+    phrase.split(/[,\-\s]/).reduce("") do |acc, word|
+      acc.concat(word.chars.fetch(0, "").upcase)
+    end
   end
 end
