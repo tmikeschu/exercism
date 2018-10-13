63619c9c2ec6939384257c084d87f04d5ce9547a
Mike Schutte <tmikeschutte@gmail.com>
2018-10-13 11:02:54 -0400

    Use String#scan on ruby acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,24 +1,10 @@
+# Acronym takes strings of words and makes capitalized acronyms.
+
 class Acronym
   def self.abbreviate(phrase)
-    new(phrase).send(:abbreviate)
-  end
-
-  attr_reader :phrase
-  def initialize(phrase)
-    @phrase = phrase
-  end
-
-  def abbreviate
     phrase.
-      split(/[,\-\s]/).
-      reduce("", &abbreviate_reducer)
-  end
-  
-  def abbreviate_reducer
-    -> acc, word { acc + first_upcase(word) }
-  end
-
-  def first_upcase(word)
-    word[0].to_s.upcase
+      scan(/\b\w/).
+      join.
+      upcase
   end
 end
