1179fa79dd46661bc1dae3da5b2e45c3a1de7507
Mike Schutte <schutte@clinc.com>
2018-10-29 21:25:25 -0400

    Move ruby tournament self. to constant

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,4 +1,10 @@
-class Tournament
+class Symbol
+  def with(*args, &block)
+    -> caller, *rest { caller.send(self, *rest, *args, &block) }
+  end
+end
+
+module Tournament
   COLUMNS = [
     ["Team", -> team, record { team }],
     ["MP", -> _, record { record.count }],
@@ -10,46 +16,43 @@ class Tournament
   NEWLINE  = $/
   OUTCOMES = {win: %i(W L), loss: %i(L W), draw: %i(D D)}
   POINTS   = {W: 3, D: 1, L: 0}
-  PADDINGS = [
-    -> x { x.ljust(31) },
-    *[-> x { x.reverse.center(4).reverse }] * 4,
-    -> x { x.rjust(3) },
-  ]
+  PIPE     = -> *fns { -> x { fns.reduce(x) { |y, fn| fn.call(y) } } }
+  FORMAT   = -> pattern { -> s { format(pattern, s) } }
+  LEFT     = FORMAT.call("%-31s")
+  CENTER   = PIPE.call(*["%3s", "%-4s"].map(&FORMAT))
+  RIGHT    = FORMAT.call("%3s")
+  PADDINGS = [ LEFT, *[CENTER] * 4, RIGHT ]
+
+  WITH_OUTCOME = -> ((*teams, outcome)) do
+    teams.zip(OUTCOMES.fetch(outcome.to_sym))
+  end
+
+  RECORD_REDUCER = -> acc, (team, outcome) do 
+    acc.update(team => [outcome]) { |_, old, new| old + new } 
+  end
+
+  CALCULATE_COLUMNS = -> team, record do
+    COLUMNS.
+      map(&:last).
+      map(&:call.with(team, record))
+  end
+
+  PADDED = -> row do 
+    PADDINGS.zip(row).
+      map(&:reduce.with(:call)).
+      join("|")
+  end
 
   def self.tally(input)
     input.
       split(NEWLINE).
-      map { |line| line.split(";") }.
-      flat_map(&with_outcome).
-      reduce({}, &record_reducer).
-      map(&calculate_columns).
+      map(&:split.with(";")).
+      flat_map(&WITH_OUTCOME).
+      reduce({}, &RECORD_REDUCER).
+      map(&CALCULATE_COLUMNS).
       sort_by { |(name, _, _,  _, _, points)| [-points, name] }.
-      dup.unshift(COLUMNS.map(&:first)).
-      map(&padded) .
+      unshift(COLUMNS.map(&:first)).
+      map(&PADDED).
       reduce("") { |acc, row| acc + row + NEWLINE }
-  end
-
-  def self.with_outcome
-    -> ((*teams, outcome)) { teams.zip(OUTCOMES.fetch(outcome.to_sym)) }
-  end
-
-  def self.record_reducer
-    -> acc, (team, outcome) do 
-      acc.update(team => [outcome]) { |_, old, new| old + new } 
-    end
-  end
-
-  def self.calculate_columns
-    -> team, record do
-      COLUMNS.map { |(_, calculator)| calculator.call(team, record) }
-    end
-  end
-
-  def self.padded
-    -> row do 
-      row.zip(PADDINGS).
-        map { |(value, padder)| padder.call(value.to_s) }.
-        join("|")
-    end
   end
 end
