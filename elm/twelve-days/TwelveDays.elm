e3f97fde55f024038bdc144aad9e9931508f7cc7
Mike Schutte <schutte@clinc.com>
2018-10-31 10:28:12 -0400

    Refactor elm twelve days

--- elm/twelve-days/TwelveDays.elm
+++ elm/twelve-days/TwelveDays.elm
@@ -1,71 +1,72 @@
 module TwelveDays exposing (recite)
 
 
-type alias Day =
-    ( Int, DayInfo )
+zipWith : (a -> b -> c) -> List a -> List b -> List c
+zipWith zipper xs ys =
+    case ( xs, ys ) of
+        ( [], _ ) ->
+            []
+
+        ( _, [] ) ->
+            []
+
+        ( x :: tx, y :: ty ) ->
+            zipper x y :: zipWith zipper tx ty
 
 
-type alias DayInfo =
-    { which : String, quantity : String, gift : String, after : Maybe String }
-
-
-days : List DayInfo
-days =
-    [ DayInfo "first" "a" "Partridge in a Pear Tree" (Just ".")
-    , DayInfo "second" "two" "Turtle Doves" (Just ", and ")
-    , DayInfo "third" "three" "French Hens" Nothing
-    , DayInfo "fourth" "four" "Calling Birds" Nothing
-    , DayInfo "fifth" "five" "Gold Rings" Nothing
-    , DayInfo "sixth" "six" "Geese-a-Laying" Nothing
-    , DayInfo "seventh" "seven" "Swans-a-Swimming" Nothing
-    , DayInfo "eighth" "eight" "Maids-a-Milking" Nothing
-    , DayInfo "ninth" "nine" "Ladies Dancing" Nothing
-    , DayInfo "tenth" "ten" "Lords-a-Leaping" Nothing
-    , DayInfo "eleventh" "eleven" "Pipers Piping" Nothing
-    , DayInfo "twelfth" "twelve" "Drummers Drumming" Nothing
+daysGifts : List ( String, String )
+daysGifts =
+    [ ( "first", "a Partridge in a Pear Tree." )
+    , ( "second", "two Turtle Doves, and " )
+    , ( "third", "three French Hens, " )
+    , ( "fourth", "four Calling Birds, " )
+    , ( "fifth", "five Gold Rings, " )
+    , ( "sixth", "six Geese-a-Laying, " )
+    , ( "seventh", "seven Swans-a-Swimming, " )
+    , ( "eighth", "eight Maids-a-Milking, " )
+    , ( "ninth", "nine Ladies Dancing, " )
+    , ( "tenth", "ten Lords-a-Leaping, " )
+    , ( "eleventh", "eleven Pipers Piping, " )
+    , ( "twelfth", "twelve Drummers Drumming, " )
     ]
+
+
+days : Int -> Int -> List String
+days offset stop =
+    daysGifts
+        |> List.map Tuple.first
+        |> List.drop offset
+        |> List.take (stop - offset)
+        |> List.map onTheNthDay
+
+
+gifts : Int -> List String
+gifts offset =
+    let
+        giftReducer el acc =
+            case acc of
+                [] ->
+                    el :: acc
+
+                h :: t ->
+                    (el ++ h) :: h :: t
+    in
+    daysGifts
+        |> List.map Tuple.second
+        |> List.foldl giftReducer []
+        |> List.reverse
+        |> List.drop offset
 
 
 recite : Int -> Int -> List String
 recite start stop =
-    List.range start stop
-        |> List.map toDay
-        |> List.map makeVerse
+    let
+        offset =
+            start - 1
+    in
+    zipWith (++) (days offset stop) (gifts offset)
 
 
-toDay : Int -> Maybe Day
-toDay x =
-    days
-        |> List.take x
-        |> List.reverse
-        |> List.head
-        |> Maybe.map (\t -> ( x, t ))
-
-
-makeVerse : Maybe Day -> String
-makeVerse day =
-    [ partOne, partTwo ]
-        |> List.map ((|>) day)
-        |> List.foldr (++) ""
-
-
-partOne : Maybe Day -> String
-partOne =
-    Maybe.map
-        (\( _, { which } ) ->
-            "On the " ++ which ++ " day of Christmas my true love gave to me, "
-        )
-        >> Maybe.withDefault ""
-
-
-partTwo : Maybe Day -> String
-partTwo =
-    Maybe.map
-        (\( num, { quantity, gift, after } ) ->
-            quantity
-                ++ " "
-                ++ gift
-                ++ Maybe.withDefault ", " after
-                ++ partTwo (toDay (num - 1))
-        )
-        >> Maybe.withDefault ""
+onTheNthDay : String -> String
+onTheNthDay whichDay =
+    "On the " ++ whichDay ++ " day of Christmas my true love gave to me, "
