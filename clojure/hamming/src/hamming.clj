51c8efd07ccd35f1d6bb4cc6b91cfe0aca7ee02a
Mike Schutte <schutte@clinc.com>
2018-11-11 14:04:10 -0500

    Refactor clojure hamming

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,12 +1,8 @@
 (ns hamming)
 
-(defn zip-to-set [a b] (map hash-set a b))
-
-(defn- mismatches [a] (not= 1 (count a)))
-
 (defn distance [& strands]
   (when (apply = (map count strands))
   (->> strands
-       (apply zip-to-set)
-       (filter mismatches)
+       (apply map =)
+       (remove identity)
        count)))
