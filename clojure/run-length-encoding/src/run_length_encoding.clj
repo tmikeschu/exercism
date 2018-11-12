d18572e5bf11b20e6ed586f2164c1ed5e0ecccfd
Mike Schutte <schutte@clinc.com>
2018-11-12 08:58:29 -0500

    Refactor clojure rle encode

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,17 +1,14 @@
 (ns run-length-encoding)
 (require '[clojure.string :as string])
 
-(defn- get-quantity [s] (Integer. (or (re-find #"\d+" s) 1)))
-(defn- get-letter [s] (re-find #"(?i)[a-z ]" s))
-
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  (->> plain-text
-       (partition-by identity)
-       (mapcat (juxt count first))
-       (filter (partial not= 1))
-       (apply str)))
+  (string/replace
+    plain-text
+    #"(.)\1+"
+    (fn [[total, unit]]
+      (str (count total) unit))))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
