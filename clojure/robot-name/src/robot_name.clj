179ae9009798f7ef59fad38024f19f52e0a7ba76
Mike Schutte <schutte@clinc.com>
2018-11-13 21:21:43 -0500

    Complete clojure robot name

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,13 +1,24 @@
 (ns robot-name)
 
-(defn robot [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn- rand-letters [n]
+  (let [letters (->> (range 65 91) (map char) (apply str))]
+    (take n (repeatedly #(rand-nth letters)))))
+(defn- rand-numbers [n] (take n (repeatedly #(rand-int 9))))
 
-(defn robot-name [robot] ;; <- arglist goes here
-      ;; your code goes here
-      )
+(defn- rand-name []
+  (apply str (concat (rand-letters 2) (rand-numbers 3))))
 
-(defn reset-name [robot] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn- valid-name [existing]
+  (->> (repeatedly rand-name)
+       (drop-while (partial contains? existing))
+       first))
+
+(def robots (atom #{}))
+
+(defn- create-name []
+  (->> (valid-name @robots)
+       ((fn [n] do (swap! robots conj n) n))))
+
+(defn robot [] (atom {:name (create-name)}))
+(defn robot-name [r] (:name @r))
+(defn reset-name [r] (swap! r assoc :name (create-name)))
