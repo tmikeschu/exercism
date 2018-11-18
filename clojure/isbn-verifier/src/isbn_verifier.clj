ec221d79fcff1bf85d51bb29163dee1ffdf5f137
Mike Schutte <schutte@clinc.com>
2018-11-18 18:09:31 -0500

    Refactor clojure isbn

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,19 +1,20 @@
-(ns isbn-verifier)
+(ns isbn-verifier
+  [:require [clojure.string :as string]])
 
 (def isbn-slots
   "This matcher captures each digit in the isbn format"
-  #"^(\d)-?(\d)(\d)(\d)-?(\d)(\d)(\d)(\d)(\d)-?([\dX])$")
+  #"^\d{9}[\dX]$")
 
 (defn isbn? [isbn]
   "Determines if isbn string is valid."
-  (let [factors (reverse (range 1 11))]
-    (true?
-      (some->> isbn
-               (re-find isbn-slots)
-               rest
-               (replace {"X" 10})
-               (map #(Integer. %))
-               (map * factors)
-               (reduce +)
-               (#(rem % 11))
-               zero?))))
+  (true?
+    (some->> (string/replace isbn #"-" "")
+             (re-find isbn-slots)
+             (replace {\X 10})
+             (map-indexed #(->> %2
+                                str
+                                Integer.
+                                (* (- 10 %1))))
+             (reduce +)
+             (#(rem % 11))
+             zero?)))
