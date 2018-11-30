73523b57df723c4d505f6441c12e1a07463fa6fc
Mike Schutte <schutte@clinc.com>
2018-11-30 16:58:56 -0500

    Refactor elm space age

--- elm/space-age/SpaceAge.elm
+++ elm/space-age/SpaceAge.elm
@@ -1,6 +1,11 @@
 module SpaceAge exposing (Planet(..), ageOn)
 
 
+ageOn : Planet -> Second -> Second
+ageOn planet seconds =
+    seconds / orbitalPeriod planet
+
+
 type Planet
     = Mercury
     | Venus
@@ -12,32 +17,40 @@ type Planet
     | Neptune
 
 
-orbitalPeriod : Planet -> Float
+type alias Second =
+    Float
+
+
+earthSecondsInYear : Second
+earthSecondsInYear =
+    31557600
+
+
+orbitalPeriod : Planet -> Second
 orbitalPeriod planet =
-    let
-        earthSecondsInYear =
-            31557600
-
-        orbitalPeriods =
-            [ ( Earth, 1 )
-            , ( Mercury, 0.2408467 )
-            , ( Venus, 0.61519726 )
-            , ( Mars, 1.8808158 )
-            , ( Jupiter, 11.862615 )
-            , ( Saturn, 29.447498 )
-            , ( Uranus, 84.016846 )
-            , ( Neptune, 164.79132 )
-            ]
-    in
     earthSecondsInYear
-        * (orbitalPeriods
-            |> List.filter (Tuple.first >> (==) planet)
-            |> List.map Tuple.second
-            |> List.head
-            |> Maybe.withDefault 0
+        * (case planet of
+            Earth ->
+                1
+
+            Mercury ->
+                0.2408467
+
+            Venus ->
+                0.61519726
+
+            Mars ->
+                1.8808158
+
+            Jupiter ->
+                11.862615
+
+            Saturn ->
+                29.447498
+
+            Uranus ->
+                84.016846
+
+            Neptune ->
+                164.79132
           )
-
-
-ageOn : Planet -> Float -> Float
-ageOn planet seconds =
-    seconds / orbitalPeriod planet
