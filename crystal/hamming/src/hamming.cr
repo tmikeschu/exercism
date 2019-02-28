b9b44bf4bf3e898dfdbe135bb49c7efd5a42950d
Mike Schutte <schutte@clinc.com>
2019-02-28 16:40:51 -0500

    Refactor crystal hamming

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -4,8 +4,8 @@ module Hamming
       raise(ArgumentError.new("Strings must be same length"))
     end
 
-    a.chars
-      .zip(b.chars)
-      .count { |(x, y)| x != y }
+    distance = 0
+    a.each_char_with_index { |c, i| distance += 1 if c != b[i] }
+    distance
   end
 end
