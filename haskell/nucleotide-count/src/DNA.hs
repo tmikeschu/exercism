5a6e0ced9c9bc20278ba97286f1f2175111ec241
Mike Schutte <schutte@clinc.com>
2019-02-28 16:14:06 -0500

    Refactor haskell nucleotide

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -3,30 +3,23 @@ module DNA
   , Nucleotide(..)
   ) where
 
-import           Data.List (foldl)
 import           Data.Map  (Map, alter, fromList)
+import           Text.Read (Read, readMaybe)
 
 data Nucleotide
   = A
   | C
   | G
   | T
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Read)
 
 base :: Map Nucleotide Int
 base = fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
 
 toNucleotide :: Char -> Maybe Nucleotide
-toNucleotide c =
-  case c of
-    'A' -> Just A
-    'C' -> Just C
-    'G' -> Just G
-    'T' -> Just T
-    _   -> Nothing
+toNucleotide c = readMaybe [c]
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts xs =
-  case mapM toNucleotide xs of
-    Nothing -> Left xs
-    Just ns -> Right . foldl (flip . alter . fmap $ succ) base $ ns
+  let nucs = mapM toNucleotide xs
+   in maybe (Left xs) (Right . foldl (flip . alter . fmap $ succ) base) nucs
