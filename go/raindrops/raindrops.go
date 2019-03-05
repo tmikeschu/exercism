24949268832e3290c5b4e46ef83ee31293aa9a1a
Mike Schutte <schutte@clinc.com>
2019-03-05 08:32:51 -0500

    Refactor go raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -5,31 +5,21 @@ import "strconv"
 
 // Convert takes a string and returns a keyword or the number as a string.
 func Convert(a int) string {
-	converters := []func(int) string{
-		modConverter(3, "Pling"),
-		modConverter(5, "Plang"),
-		modConverter(7, "Plong")}
-
 	final := ""
 
-	for _, c := range converters {
-		conversion := c(a)
-		if conversion != "" {
-			final = final + conversion
-		}
+	if a%3 == 0 {
+		final += "Pling"
+	}
+	if a%5 == 0 {
+		final += "Plang"
+	}
+	if a%7 == 0 {
+		final += "Plong"
 	}
 
 	if final == "" {
 		final = strconv.Itoa(a)
 	}
-	return final
-}
 
-func modConverter(i int, s string) func(int) string {
-	return func(x int) string {
-		if x%i == 0 {
-			return s
-		}
-		return ""
-	}
+	return final
 }
