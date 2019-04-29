9bd9bdd96ae4c50f99d4824bd10371ecf13895cb
Mike Schutte <schutte@clinc.com>
2019-04-29 14:34:25 -0600

    Refactor rust str reverse

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,9 +1,3 @@
 pub fn reverse(input: &str) -> String {
-    let mut output = String::with_capacity(input.len());
-
-    for c in input.chars() {
-        output.insert_str(0, &c.to_string())
-    }
-
-    output
+    input.chars().rev().collect()
 }
