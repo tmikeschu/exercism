18705f2dd86f5b76c5a066111004530da0ef628a
Mike Schutte <tmikeschutte@gmail.com>
2019-05-13 08:21:53 -0600

    Refactor rust bob with boxed fns

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,47 +1,57 @@
 use regex::Regex;
 
 const SHOUT: &str = "SHOUT";
+const SHOUT_RESPONSE: &str = "Whoa, chill out!";
+
 const QUESTION: &str = "QUESTION";
+const QUESTION_RESPONSE: &str = "Sure.";
+
 const SHOUTING_QUESTION: &str = "SHOUTQUESTION";
+const SHOUTING_QUESTION_RESPONSE: &str = "Calm down, I know what I'm doing!";
+
 const SILENT: &str = "SILENT";
+const SILENT_RESPONSE: &str = "Fine. Be that way!";
 
-fn shout(s: &str) -> &str {
-    if Regex::new(r"[A-Z]").unwrap().is_match(s) && s.to_uppercase() == s {
-        SHOUT
-    } else {
-        ""
-    }
-}
-
-fn question(s: &str) -> &str {
-    if s.trim().ends_with("?") {
-        QUESTION
-    } else {
-        ""
-    }
-}
-
-fn silence(s: &str) -> &str {
-    if s.trim() == "" {
-        SILENT
-    } else {
-        ""
-    }
-}
+const DEFAULT_RESPONSE: &str = "Whatever.";
 
 pub fn reply(message: &str) -> &str {
-    let classification: &str = &format!(
-        "{}{}{}",
-        shout(message),
-        question(message),
-        silence(message)
-    );
+    let classification_classifiers: Vec<(&str, Box<Fn(&str) -> bool>)> = vec![
+        (SHOUTING_QUESTION, Box::new(is_shouting_question)),
+        (SHOUT, Box::new(is_shout)),
+        (QUESTION, Box::new(is_question)),
+        (SILENT, Box::new(is_silent)),
+    ];
 
-    return match classification {
-        SHOUTING_QUESTION => "Calm down, I know what I'm doing!",
-        SHOUT => "Whoa, chill out!",
-        QUESTION => "Sure.",
-        SILENT => "Fine. Be that way!",
-        _ => "Whatever.",
+    get_response(
+        classification_classifiers
+            .iter()
+            .find(|(_, classifier)| classifier(message))
+            .map_or("", |(classification, _)| classification),
+    )
+}
+
+fn is_shout(s: &str) -> bool {
+    Regex::new(r"[A-Z]").unwrap().is_match(s) && s.to_uppercase() == s
+}
+
+fn is_question(s: &str) -> bool {
+    s.trim().ends_with("?")
+}
+
+fn is_silent(s: &str) -> bool {
+    s.trim().is_empty()
+}
+
+fn is_shouting_question(s: &str) -> bool {
+    is_shout(s) && is_question(s)
+}
+
+fn get_response(s: &str) -> &str {
+    return match s {
+        SHOUTING_QUESTION => SHOUTING_QUESTION_RESPONSE,
+        SHOUT => SHOUT_RESPONSE,
+        QUESTION => QUESTION_RESPONSE,
+        SILENT => SILENT_RESPONSE,
+        _ => DEFAULT_RESPONSE,
     };
 }
