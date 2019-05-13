88b579763cff6df49b05e155e0c235489a7b7637
Mike Schutte <tmikeschutte@gmail.com>
2019-05-12 22:46:57 -0600

    Try different rust bob strategy

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,23 +1,47 @@
 use regex::Regex;
 
-fn is_shouting(s: &str) -> bool {
-    Regex::new(r"[A-Z]").unwrap().is_match(s) && s.to_uppercase() == s
+const SHOUT: &str = "SHOUT";
+const QUESTION: &str = "QUESTION";
+const SHOUTING_QUESTION: &str = "SHOUTQUESTION";
+const SILENT: &str = "SILENT";
+
+fn shout(s: &str) -> &str {
+    if Regex::new(r"[A-Z]").unwrap().is_match(s) && s.to_uppercase() == s {
+        SHOUT
+    } else {
+        ""
+    }
 }
 
-fn is_asking(s: &str) -> bool {
-    s.trim().ends_with("?")
+fn question(s: &str) -> &str {
+    if s.trim().ends_with("?") {
+        QUESTION
+    } else {
+        ""
+    }
+}
+
+fn silence(s: &str) -> &str {
+    if s.trim() == "" {
+        SILENT
+    } else {
+        ""
+    }
 }
 
 pub fn reply(message: &str) -> &str {
-    if message.trim() == "" {
-        "Fine. Be that way!"
-    } else if is_shouting(message) && is_asking(message) {
-        "Calm down, I know what I'm doing!"
-    } else if is_shouting(message) {
-        "Whoa, chill out!"
-    } else if is_asking(message) {
-        "Sure."
-    } else {
-        "Whatever."
-    }
+    let classification: &str = &format!(
+        "{}{}{}",
+        shout(message),
+        question(message),
+        silence(message)
+    );
+
+    return match classification {
+        SHOUTING_QUESTION => "Calm down, I know what I'm doing!",
+        SHOUT => "Whoa, chill out!",
+        QUESTION => "Sure.",
+        SILENT => "Fine. Be that way!",
+        _ => "Whatever.",
+    };
 }
