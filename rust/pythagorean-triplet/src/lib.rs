bcd7bd4ababa5883532013103fcc848037eb2e51
Mike Schutte <tmikeschutte@gmail.com>
2019-05-21 16:45:53 -0400

    Redo rust pythagorean

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,22 +1,23 @@
-pub fn find() -> Option<u32> {
-    for a in 200..600 {
-        for b in 200..600 {
-            for c in 282..600 {
-                if a + b + c == 1000 {
-                    if is_triplet(a, b, c) {
-                        return Some(a * b * c);
-                    }
-                }
-            }
-        }
-    }
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use std::collections::HashSet;
 
-    None
+pub fn find(n: u32) -> HashSet<[u32; 3]> {
+    (1..(n / 3))
+        .into_par_iter()
+        .filter_map(|a| {
+            let bc = n - a;
+            let b = (bc.pow(2) - a.pow(2)) / (bc * 2);
+            let c = bc - b;
+
+            if is_triplet(a, b, c) {
+                Some([a, b, c])
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<_>>()
 }
 
 fn is_triplet(a: u32, b: u32, c: u32) -> bool {
-    let a2 = a.pow(2);
-    let b2 = b.pow(2);
-    let c2 = c.pow(2);
-    a2 + b2 == c2 || b2 + c2 == a2 || a2 + c2 == b2
+    a < b && a.pow(2) + b.pow(2) == c.pow(2)
 }
