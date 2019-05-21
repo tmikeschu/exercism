49429225b1085207ae8a5f51f4d914de6a087926
Mike Schutte <tmikeschutte@gmail.com>
2019-05-21 12:45:24 -0400

    Refactor rust matching brackets

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -1,29 +1,32 @@
-const OPENERS: &str = "{([";
-const CLOSERS: &str = "})]";
-const BRACKETS: &str = "{}()[]";
-
 pub fn brackets_are_balanced(string: &str) -> bool {
     let mut openers = Vec::new();
 
     for c in string.chars() {
-        if OPENERS.contains(c) {
-            openers.push(c);
-        } else if CLOSERS.contains(c)
-            && openers
-                .pop()
-                .and_then(closer_for)
-                .filter(|expected_closer| *expected_closer == c)
-                .is_none()
-        {
-            return false;
+        match c {
+            '(' | '[' | '{' => openers.push(c),
+            ')' | ']' | '}' => {
+                if is_not_balanced(&mut openers, c) {
+                    return false;
+                }
+            }
+            _ => (),
         }
     }
 
     openers.is_empty()
 }
 
-fn closer_for(bracket: char) -> Option<char> {
+fn is_not_balanced(openers: &mut Vec<char>, closer: char) -> bool {
+    openers
+        .pop()
+        .and_then(closer_for)
+        .filter(|expected| *expected == closer)
+        .is_none()
+}
+
+const BRACKETS: &str = "()[]{}";
+fn closer_for(opener: char) -> Option<char> {
     BRACKETS
-        .find(bracket)
+        .find(opener)
         .and_then(|index| BRACKETS.chars().skip(index + 1).next())
 }
