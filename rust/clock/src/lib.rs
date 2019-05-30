a39b5f29a989fef0e16aab446a4e30d0d35fa952
Mike Schutte <tmikeschutte@gmail.com>
2019-05-30 10:32:51 -0400

    Use deps on rust clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,3 +1,5 @@
+use modulo::Mod;
+use num_integer::Integer;
 use std::fmt;
 
 const MINUTES_PER_HOUR: i32 = 60;
@@ -11,27 +13,18 @@ pub struct Clock {
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Clock {
-            minutes: Clock::sanitize_minutes(minutes + MINUTES_PER_HOUR * hours),
+            minutes: (minutes + MINUTES_PER_HOUR * hours).modulo(HOURS_PER_DAY),
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         Clock::new(0, self.minutes + minutes)
     }
-
-    fn sanitize_minutes(minutes: i32) -> i32 {
-        if minutes < 0 {
-            Clock::sanitize_minutes(HOURS_PER_DAY + minutes)
-        } else {
-            minutes % HOURS_PER_DAY
-        }
-    }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let hours = self.minutes / MINUTES_PER_HOUR;
-        let minutes = self.minutes % MINUTES_PER_HOUR;
+        let (hours, minutes) = self.minutes.div_rem(&MINUTES_PER_HOUR);
         write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
