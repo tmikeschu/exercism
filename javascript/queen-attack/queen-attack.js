81847651f958bad253b05aebd16e2cbb4a8bc27a
Mike Schutte <tmikeschutte@gmail.com>
2019-06-20 09:05:19 -0400

    Refactor js queen attack

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -1,56 +1,61 @@
 // formatting
-const EMPTY_CELL = "_"
-const CELL_SEPARATOR = " "
-const NEWLINE = "\n"
-const WHITE = "W"
-const BLACK = "B"
+const EMPTY_CELL = "_";
+const CELL_SEPARATOR = " ";
+const NEWLINE = "\n";
+const WHITE = "W";
+const BLACK = "B";
 
-// utils
-const ifElse = (test, ifTrue, ifFalse) => x => test(x) ? ifTrue(x) : ifFalse(x)
-const id = x => x
-const always = a => () => a
-const eq = a => b => a === b
-const map = mapper => xs => xs.map(mapper)
-const join = del => xs => xs.join(del)
-const pipe = (...fns) => x => fns.reduce((y, fn) => fn(y), x)
-const concat = a => b => b.concat(a)
-const arrsEq = (as, bs) => as.every((a, i) => eq(a)(bs[i]))
-const defaultTo = dflt => ifElse(eq(undefined), always(dflt),  id)
-const slope = ([x1, y1], [x2, y2]) => Math.abs((y1 - y2) / (x1 - x2))
-
+const Utils = {
+  id: x => x,
+  always: a => () => a,
+  eq: a => b => a === b,
+  map: mapper => xs => xs.map(mapper),
+  join: del => xs => xs.join(del),
+  pipe: (...fns) => x => fns.reduce((y, fn) => fn(y), x),
+  concat: a => b => b.concat(a),
+  arrsEq: (as, bs) => as.every((a, i) => Utils.eq(a)(bs[i])),
+  defaultTo: dflt =>
+    Utils.ifElse(Utils.eq(undefined), Utils.always(dflt), Utils.id),
+  slope: ([x1, y1], [x2, y2]) => Math.abs((y1 - y2) / (x1 - x2)),
+  ifElse: (test, ifTrue, ifFalse) => x => (test(x) ? ifTrue(x) : ifFalse(x))
+};
 
 // model
-const initialBoard = [...Array(8)].map(() => [...Array(8)])
+const initialBoard = [...Array(8)].map(() => [...Array(8)]);
 const populateBoard = ({ white, black }, board = initialBoard) => {
-  return board.map(
-    (row, i) => row.map(
-      (cell, j) => {
-        if (arrsEq([i, j], white)) {
-          return WHITE
-        } else if (arrsEq([i, j], black)) {
-          return BLACK
-        }
-        return cell
+  const { arrsEq } = Utils;
+
+  return board.map((row, i) =>
+    row.map((cell, j) => {
+      const current = [i, j];
+
+      if (arrsEq(current, white)) {
+        return WHITE;
+      } else if (arrsEq(current, black)) {
+        return BLACK;
       }
-    )
-  )
-}
+      return cell;
+    })
+  );
+};
 
 export class QueenAttack {
   constructor({ white, black } = { white: [0, 3], black: [7, 3] }) {
-    this.validate({ white, black })
-    this.white = white
-    this.black = black
-    this.board = populateBoard({ white, black })
+    this.validate({ white, black });
+    this.white = white;
+    this.black = black;
+    this.board = populateBoard({ white, black });
   }
 
   validate({ white, black }) {
-    if (arrsEq(white, black)) {
-      throw "Queens cannot share the same space"
+    if (Utils.arrsEq(white, black)) {
+      throw "Queens cannot share the same space";
     }
   }
 
   toString() {
+    const { map, pipe, join, defaultTo, concat } = Utils;
+
     return pipe(
       map(
         pipe(
@@ -60,21 +65,18 @@ export class QueenAttack {
       ),
       join(NEWLINE),
       concat(NEWLINE)
-    )(this.board)
+    )(this.board);
   }
 
   canAttack() {
-    const  {
+    const { eq, slope } = Utils;
+    const {
       white,
       black,
       white: [wRow, wCol],
-      black: [bRow, bCol],
-    } = this
+      black: [bRow, bCol]
+    } = this;
 
-    return [
-      eq(wRow)(bRow),
-      eq(wCol)(bCol),
-      eq(1)(slope(white, black))
-    ].some(Boolean)
+    return [wRow / bRow, wCol / bCol, slope(white, black)].some(eq(1));
   }
 }
