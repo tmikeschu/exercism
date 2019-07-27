107f6bf1e40c78659f74c871e04d3fa85cf0fbef
Mike Schutte <tmikeschutte@gmail.com>
2019-07-27 13:41:32 -0400

    Complete rust anagram

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,21 @@
+use std::collections::HashSet;
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let lowered = word.to_lowercase();
+    let sorted = sort(&lowered);
+
+    possible_anagrams
+        .iter()
+        .filter(|&&possible_anagram| {
+            let lowered2 = possible_anagram.to_lowercase();
+            sort(&lowered2) == sorted && lowered2 != lowered
+        })
+        .cloned()
+        .collect()
+}
+
+fn sort(s: &str) -> Vec<char> {
+    let mut letters = s.chars().collect::<Vec<char>>();
+    letters.sort();
+    letters
+}
