83fceed2d67c2c6ca247f0aa5b632c3332176a41
Mike Schutte <tmikeschutte@gmail.com>
2019-10-13 13:54:40 -0400

    Refactor elixir bracket push

--- elixir/bracket-push/lib/bracket_push.ex
+++ elixir/bracket-push/lib/bracket_push.ex
@@ -1,33 +1,29 @@
 defmodule BracketPush do
+  @openers ~w([ { ()
+  @closers ~w(] } \))
+  @matches ~w([] {} (\))
+
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
   @spec check_brackets(String.t()) :: boolean
-  def check_brackets(<<x::utf8>> <> _) when <<x::utf8>> in ["]", "}", ")"], do: false
-
   def check_brackets(str) do
-    pairs = %{"[" => "]", "{" => "}", "(" => ")"}
-
     str
     |> String.split("", trim: true)
-    |> Enum.reduce_while([], fn el, acc ->
-      case el do
-        open when open in ~w([ { () ->
-          {:cont, [open | acc]}
-
-        close when close in ~w(] } \)) ->
-          [h | tail] = acc
-
-          if Map.get(pairs, h) == el do
-            {:cont, tail}
-          else
-            {:halt, [:error]}
-          end
-
-        _ ->
-          {:cont, acc}
-      end
-    end)
+    |> Enum.reduce_while([], &reducer/2)
     |> Enum.empty?()
   end
+
+  defp reducer(_, [close]) when close in @closers, do: {:halt, [:error]}
+
+  defp reducer(open, acc) when open in @openers, do: {:cont, [open | acc]}
+
+  defp reducer(close, [head | tail]) when close in @closers do
+    case head <> close do
+      match when match in @matches -> {:cont, tail}
+      _ -> {:halt, [:error]}
+    end
+  end
+
+  defp reducer(_, acc), do: {:cont, acc}
 end
