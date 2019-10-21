12a246090cd331d3a049066f527d77d1876af38a
Mike Schutte <tmikeschutte@gmail.com>
2019-10-21 07:55:28 -0400

    Refactor elixir linked list

--- elixir/simple-linked-list/lib/linked_list.ex
+++ elixir/simple-linked-list/lib/linked_list.ex
@@ -5,7 +5,7 @@ defmodule LinkedList do
   Construct a new LinkedList
   """
   @spec new() :: t
-  def new(), do: {nil, nil}
+  def new(), do: {}
 
   @doc """
   Push an item onto a LinkedList
@@ -17,37 +17,37 @@ defmodule LinkedList do
   Calculate the length of a LinkedList
   """
   @spec length(t) :: non_neg_integer()
-  def length({nil, nil}), do: 0
+  def length({}), do: 0
   def length({_, t}), do: length(t, 1)
-  def length({nil, nil}, n), do: n
+  def length({}, n), do: n
   def length({_, t}, n), do: length(t, n + 1)
 
   @doc """
   Determine if a LinkedList is empty
   """
   @spec empty?(t) :: boolean()
-  def empty?({nil, nil}), do: true
+  def empty?({}), do: true
   def empty?(_), do: false
 
   @doc """
   Get the value of a head of the LinkedList
   """
   @spec peek(t) :: {:ok, any()} | {:error, :empty_list}
-  def peek({nil, nil}), do: {:error, :empty_list}
+  def peek({}), do: {:error, :empty_list}
   def peek({head, _}), do: {:ok, head}
 
   @doc """
   Get tail of a LinkedList
   """
   @spec tail(t) :: {:ok, t} | {:error, :empty_list}
-  def tail({nil, nil}), do: {:error, :empty_list}
+  def tail({}), do: {:error, :empty_list}
   def tail({_, tail}), do: {:ok, tail}
 
   @doc """
   Remove the head from a LinkedList
   """
   @spec pop(t) :: {:ok, any(), t} | {:error, :empty_list}
-  def pop({nil, nil}), do: {:error, :empty_list}
+  def pop({}), do: {:error, :empty_list}
   def pop({head, tail}), do: {:ok, head, tail}
 
   @doc """
@@ -62,7 +62,7 @@ defmodule LinkedList do
   Construct a stdlib List LinkedList from a LinkedList
   """
   @spec to_list(t) :: list()
-  def to_list({nil, nil}), do: []
+  def to_list({}), do: []
   def to_list({h, tail}), do: [h | to_list(tail)]
 
   @doc """
@@ -70,6 +70,6 @@ defmodule LinkedList do
   """
   @spec reverse(t) :: t
   def reverse(list), do: reverse(list, LinkedList.new())
-  def reverse({nil, nil}, acc), do: acc
+  def reverse({}, acc), do: acc
   def reverse({h, tail}, acc), do: reverse(tail, {h, acc})
 end
