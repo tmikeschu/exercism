4e0437914c65c8f8363d1cb06de3aa4cda42b208
Mike Schutte <tmikeschutte@gmail.com>
2021-02-17 13:47:23 -0500

    foldLeft -> map tuples

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,11 +1,7 @@
 object Etl {
-  def transform(letters: Map[Int, Seq[String]]): Map[String, Int] = {
-    letters.flatMap {
-      case ((value, letters)) => {
-        letters.foldLeft(Map.empty[String, Int]) { case (acc2, letter) =>
-          acc2 + (letter.toLowerCase -> value)
-        }
-      }
+  def transform(scoresLetters: Map[Int, Seq[String]]): Map[String, Int] = {
+    scoresLetters.flatMap { case (score, letters) =>
+      letters.map(letter => (letter.toLowerCase, score))
     }
   }
 }
