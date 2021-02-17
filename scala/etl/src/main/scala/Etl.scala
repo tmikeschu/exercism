b249cdda67271f380575958a16cc39fbc9fb0daa
Mike Schutte <tmikeschutte@gmail.com>
2021-02-17 13:36:32 -0500

    foldLeft -> flatMap

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,8 +1,8 @@
 object Etl {
   def transform(letters: Map[Int, Seq[String]]): Map[String, Int] = {
-    letters.foldLeft(Map.empty[String, Int]) {
-      case (acc, (value, letters)) => {
-        acc ++ letters.foldLeft(Map.empty[String, Int]) { case (acc2, letter) =>
+    letters.flatMap {
+      case ((value, letters)) => {
+        letters.foldLeft(Map.empty[String, Int]) { case (acc2, letter) =>
           acc2 + (letter.toLowerCase -> value)
         }
       }
